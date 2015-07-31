937c5e75435e108afc5bd6bfa5b03a32df09bc93
Aleksandr Drozd <alexander.drozd@gmail.com>
2015-07-31 14:00:57 +0900

    added axvlines_color parameter to pandas.tools.plotting.parallel_coordinates

--- pandas/tools/plotting.py
+++ pandas/tools/plotting.py
@@ -595,7 +595,7 @@ def bootstrap_plot(series, fig=None, size=50, samples=500, **kwds):
 @deprecate_kwarg(old_arg_name='data', new_arg_name='frame')
 def parallel_coordinates(frame, class_column, cols=None, ax=None, color=None,
                          use_columns=False, xticks=None, colormap=None,
-                         axvlines=True, **kwds):
+                         axvlines=True, axvlines_color='black', **kwds):
     """Parallel coordinates plotting.
 
     Parameters
@@ -617,6 +617,8 @@ def parallel_coordinates(frame, class_column, cols=None, ax=None, color=None,
         Colormap to use for line colors.
     axvlines: bool, optional
         If true, vertical lines will be added at each xtick
+    axvlines_color: str, optional
+        Color of the vertical lines
     kwds: keywords
         Options to pass to matplotlib plotting method
 
@@ -683,7 +685,7 @@ def parallel_coordinates(frame, class_column, cols=None, ax=None, color=None,
 
     if axvlines:
         for i in x:
-            ax.axvline(i, linewidth=1, color='black')
+            ax.axvline(i, linewidth=1, color=axvlines_color)
 
     ax.set_xticks(x)
     ax.set_xticklabels(df.columns)
